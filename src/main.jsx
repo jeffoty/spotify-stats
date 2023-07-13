4b235facdcdb5781570ab271f4bff24cce24f33e
waikau maina <ericmaina.dev@gmail.com>
2023-07-13 13:46:56 +0300

    added react router and setup the root router

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom"
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>
+  }
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
