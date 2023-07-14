a4c404ca715fcc8a60ff0c97ec2fcb890b2b02af
jeffoty <odhiambo47@gmail.com>
2023-07-14 10:58:10 +0300

    ✨ feat: login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import Home from './components/Home'
+// import Home from './components/Home'
+import Login from './components/Login'
 
 function App() {
 
   return (
-    <Home />
+   < Login/>
   )
 }
 
