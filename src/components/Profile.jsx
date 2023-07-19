6ec87de0b82bad47c32b5239547a1ae1d4844be6
jeffoty <odhiambo47@gmail.com>
2023-07-19 15:38:04 +0300

    minor changes

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { getAccessToken, getFollowing, getPlaylists, getUser } from '../services'
 
 function Profile() {
