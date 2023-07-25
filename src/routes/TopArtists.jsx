26f2cf5d1e521074cf26fc4dc42ad97de9b5a349
waikau maina <ericmaina.dev@gmail.com>
2023-07-25 13:16:16 +0300

    setup files for fetching top artists

---
+++ src/routes/TopArtists.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react'
+
+function TopArtists() {
+  const [topArtists, setTopArtists] = useState(null)
+
+  useEffect(() => {
+    
+  }, [])
+
+  return (
+    <div>TopArtists</div>
+  )
+}
+
+export default TopArtists
