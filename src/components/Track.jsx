b416a198aa2abda8bce2bec075b2a1fc2d829775
waikau maina <ericmaina.dev@gmail.com>
2023-07-28 14:15:48 +0300

    added the album name to the track details

--- src/components/Track.jsx
+++ src/components/Track.jsx
@@ -8,9 +8,10 @@ function Track({track}) {
 
   return (
     <div className='track-panel'>
-        <h2>{track?.name}</h2>
-        <img className='track-img' src={track?.album.images[2].url} alt="" loading='lazy'/>
+        <a href={track?.external_urls.spotify} target='_blank'>{track?.name}</a>
+        <a href={track?.external_urls.spotify} target="_blank" rel="noopener noreferrer"><img className='track-img' src={track?.album.images[2].url} alt="" loading='lazy'/></a>
         <p>{ combineArtists(track?.artists) }</p>
+        <p>{ track?.album.name }</p>
         <p>{ getDuration(track?.duration_ms) }</p>
     </div>
   )
